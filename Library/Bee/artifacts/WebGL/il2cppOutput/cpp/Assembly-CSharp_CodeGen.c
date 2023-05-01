77dcfb4f99f6693a03931f4c4b4995fdf3dbad67
CarstenChen <ztcarsten@gmail.com>
2023-05-01 04:17:01 +0100

    Update
    Update

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,185 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void GameManager::Start()
+extern void GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2 (void);
+// 0x00000002 System.Void GameManager::Update()
+extern void GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41 (void);
+// 0x00000003 System.Void GameManager::ClickBag()
+extern void GameManager_ClickBag_mD4C10FE2F3A4559F66510516FAC762587FC02314 (void);
+// 0x00000004 System.Collections.IEnumerator GameManager::QuitGame(System.Single)
+extern void GameManager_QuitGame_mF37D199F198F71A6CB86F3BB3B6F20BDE9999DA0 (void);
+// 0x00000005 System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x00000006 System.Void GameManager/<QuitGame>d__8::.ctor(System.Int32)
+extern void U3CQuitGameU3Ed__8__ctor_m8795C9CA65E4A2855F3900781E385D98ECDCF3B8 (void);
+// 0x00000007 System.Void GameManager/<QuitGame>d__8::System.IDisposable.Dispose()
+extern void U3CQuitGameU3Ed__8_System_IDisposable_Dispose_m2C37C7BE46B18726D97D6B067BDAA9AD0DB170CD (void);
+// 0x00000008 System.Boolean GameManager/<QuitGame>d__8::MoveNext()
+extern void U3CQuitGameU3Ed__8_MoveNext_m63EC2FDC844A5DDD6FF684F8B3A7B9406B61E5F3 (void);
+// 0x00000009 System.Object GameManager/<QuitGame>d__8::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CQuitGameU3Ed__8_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA3259A6417B2FEFD54B84F5B2A250BA5A23AF6D6 (void);
+// 0x0000000A System.Void GameManager/<QuitGame>d__8::System.Collections.IEnumerator.Reset()
+extern void U3CQuitGameU3Ed__8_System_Collections_IEnumerator_Reset_mE4C99498B8EC451EB7A35F6A0F84371AC989C6C9 (void);
+// 0x0000000B System.Object GameManager/<QuitGame>d__8::System.Collections.IEnumerator.get_Current()
+extern void U3CQuitGameU3Ed__8_System_Collections_IEnumerator_get_Current_mA12A869A0BB83DE3E69F2649E8D3F9B4FDF2A3A6 (void);
+// 0x0000000C System.Void Dialogue::.ctor()
+extern void Dialogue__ctor_m0FCF351AA72166AF74CE3A4F57EBD8287702FAC7 (void);
+// 0x0000000D DialogueManager DialogueManager::get_Instance()
+extern void DialogueManager_get_Instance_mCC30E793B6A9FCD26A0DCDF92A0B5C015F784FCC (void);
+// 0x0000000E System.Void DialogueManager::set_Instance(DialogueManager)
+extern void DialogueManager_set_Instance_m075924916728C4D3A7F84FCEE902814E07ABF728 (void);
+// 0x0000000F System.Void DialogueManager::Awake()
+extern void DialogueManager_Awake_m1E03D7AA32F5A9A8F8FBE2EC1C9ABB2742341207 (void);
+// 0x00000010 System.Void DialogueManager::Start()
+extern void DialogueManager_Start_m4C2D94B2DD22F681BF418599E65069676ABF4750 (void);
+// 0x00000011 System.Void DialogueManager::DisplayLine(System.Int32,System.Int32)
+extern void DialogueManager_DisplayLine_mD1972B64334CD20C957ACCF7107F9DBF1A6D5F55 (void);
+// 0x00000012 System.Collections.IEnumerator DialogueManager::WaitSoundEndToNextLine(Dialogue)
+extern void DialogueManager_WaitSoundEndToNextLine_mBACF9D799274D9E7E886B95F375B14FB88774711 (void);
+// 0x00000013 System.Collections.IEnumerator DialogueManager::SetLineUI(System.Boolean,System.Single)
+extern void DialogueManager_SetLineUI_m3663B82341C95D0BC0ECFBBCB6712431EABE3B95 (void);
+// 0x00000014 System.Void DialogueManager::.ctor()
+extern void DialogueManager__ctor_m219FBB101CA1818E97E7E97784E88402F20AE430 (void);
+// 0x00000015 System.Void DialogueManager/<WaitSoundEndToNextLine>d__14::.ctor(System.Int32)
+extern void U3CWaitSoundEndToNextLineU3Ed__14__ctor_m5EBE99407402E70E17F337F18149960B34770A6A (void);
+// 0x00000016 System.Void DialogueManager/<WaitSoundEndToNextLine>d__14::System.IDisposable.Dispose()
+extern void U3CWaitSoundEndToNextLineU3Ed__14_System_IDisposable_Dispose_m96CC7C5C2FCF9D0AED0A99592A103A56E097DDBF (void);
+// 0x00000017 System.Boolean DialogueManager/<WaitSoundEndToNextLine>d__14::MoveNext()
+extern void U3CWaitSoundEndToNextLineU3Ed__14_MoveNext_m145C02D430335CEAF65EABEDFA5B722C7E04417A (void);
+// 0x00000018 System.Object DialogueManager/<WaitSoundEndToNextLine>d__14::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CWaitSoundEndToNextLineU3Ed__14_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m93D19D0B5564448B6BEFED9554971B884C555D97 (void);
+// 0x00000019 System.Void DialogueManager/<WaitSoundEndToNextLine>d__14::System.Collections.IEnumerator.Reset()
+extern void U3CWaitSoundEndToNextLineU3Ed__14_System_Collections_IEnumerator_Reset_mBEFAAAC0ABDAD3488B2B28ADCB8117847F148288 (void);
+// 0x0000001A System.Object DialogueManager/<WaitSoundEndToNextLine>d__14::System.Collections.IEnumerator.get_Current()
+extern void U3CWaitSoundEndToNextLineU3Ed__14_System_Collections_IEnumerator_get_Current_m00E2297E51CEAF0E4CA0BFCA704EFE745301BDDC (void);
+// 0x0000001B System.Void DialogueManager/<SetLineUI>d__15::.ctor(System.Int32)
+extern void U3CSetLineUIU3Ed__15__ctor_mEE66DBAB8181EE9C0332E41E3527A48F2C5D9078 (void);
+// 0x0000001C System.Void DialogueManager/<SetLineUI>d__15::System.IDisposable.Dispose()
+extern void U3CSetLineUIU3Ed__15_System_IDisposable_Dispose_mBD12D04E12E9C4A20C5295319F2A53486E2DD8EC (void);
+// 0x0000001D System.Boolean DialogueManager/<SetLineUI>d__15::MoveNext()
+extern void U3CSetLineUIU3Ed__15_MoveNext_m3CEAE3266F236FB42D867E90A9FF6FD8CA2E6404 (void);
+// 0x0000001E System.Object DialogueManager/<SetLineUI>d__15::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CSetLineUIU3Ed__15_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m62CCD0B02BC37AF8151212C7B46F22D6D31EF07F (void);
+// 0x0000001F System.Void DialogueManager/<SetLineUI>d__15::System.Collections.IEnumerator.Reset()
+extern void U3CSetLineUIU3Ed__15_System_Collections_IEnumerator_Reset_m82D35F87248202C9327225D430BA13439A5FE2B6 (void);
+// 0x00000020 System.Object DialogueManager/<SetLineUI>d__15::System.Collections.IEnumerator.get_Current()
+extern void U3CSetLineUIU3Ed__15_System_Collections_IEnumerator_get_Current_m7B4F53741CB4716890E040FC7D3684CE95962B88 (void);
+// 0x00000021 System.Void PlotManager::.ctor()
+extern void PlotManager__ctor_m728BB55B53CFA5F5B6B18CE491BCB2665E3B1D63 (void);
+// 0x00000022 System.Void DialogueSticker::Start()
+extern void DialogueSticker_Start_m8E409FBE628D9A175347F57A451E8F98069723D8 (void);
+// 0x00000023 System.Void DialogueSticker::Update()
+extern void DialogueSticker_Update_mD853E01077D7869A24C1345DA5889614A37D6BC0 (void);
+// 0x00000024 System.Void DialogueSticker::.ctor()
+extern void DialogueSticker__ctor_mC948229D86FAF95E4FF7FD8EF1C09D14CC3A2EB1 (void);
+static Il2CppMethodPointer s_methodPointers[36] = 
+{
+	GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2,
+	GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41,
+	GameManager_ClickBag_mD4C10FE2F3A4559F66510516FAC762587FC02314,
+	GameManager_QuitGame_mF37D199F198F71A6CB86F3BB3B6F20BDE9999DA0,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	U3CQuitGameU3Ed__8__ctor_m8795C9CA65E4A2855F3900781E385D98ECDCF3B8,
+	U3CQuitGameU3Ed__8_System_IDisposable_Dispose_m2C37C7BE46B18726D97D6B067BDAA9AD0DB170CD,
+	U3CQuitGameU3Ed__8_MoveNext_m63EC2FDC844A5DDD6FF684F8B3A7B9406B61E5F3,
+	U3CQuitGameU3Ed__8_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA3259A6417B2FEFD54B84F5B2A250BA5A23AF6D6,
+	U3CQuitGameU3Ed__8_System_Collections_IEnumerator_Reset_mE4C99498B8EC451EB7A35F6A0F84371AC989C6C9,
+	U3CQuitGameU3Ed__8_System_Collections_IEnumerator_get_Current_mA12A869A0BB83DE3E69F2649E8D3F9B4FDF2A3A6,
+	Dialogue__ctor_m0FCF351AA72166AF74CE3A4F57EBD8287702FAC7,
+	DialogueManager_get_Instance_mCC30E793B6A9FCD26A0DCDF92A0B5C015F784FCC,
+	DialogueManager_set_Instance_m075924916728C4D3A7F84FCEE902814E07ABF728,
+	DialogueManager_Awake_m1E03D7AA32F5A9A8F8FBE2EC1C9ABB2742341207,
+	DialogueManager_Start_m4C2D94B2DD22F681BF418599E65069676ABF4750,
+	DialogueManager_DisplayLine_mD1972B64334CD20C957ACCF7107F9DBF1A6D5F55,
+	DialogueManager_WaitSoundEndToNextLine_mBACF9D799274D9E7E886B95F375B14FB88774711,
+	DialogueManager_SetLineUI_m3663B82341C95D0BC0ECFBBCB6712431EABE3B95,
+	DialogueManager__ctor_m219FBB101CA1818E97E7E97784E88402F20AE430,
+	U3CWaitSoundEndToNextLineU3Ed__14__ctor_m5EBE99407402E70E17F337F18149960B34770A6A,
+	U3CWaitSoundEndToNextLineU3Ed__14_System_IDisposable_Dispose_m96CC7C5C2FCF9D0AED0A99592A103A56E097DDBF,
+	U3CWaitSoundEndToNextLineU3Ed__14_MoveNext_m145C02D430335CEAF65EABEDFA5B722C7E04417A,
+	U3CWaitSoundEndToNextLineU3Ed__14_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m93D19D0B5564448B6BEFED9554971B884C555D97,
+	U3CWaitSoundEndToNextLineU3Ed__14_System_Collections_IEnumerator_Reset_mBEFAAAC0ABDAD3488B2B28ADCB8117847F148288,
+	U3CWaitSoundEndToNextLineU3Ed__14_System_Collections_IEnumerator_get_Current_m00E2297E51CEAF0E4CA0BFCA704EFE745301BDDC,
+	U3CSetLineUIU3Ed__15__ctor_mEE66DBAB8181EE9C0332E41E3527A48F2C5D9078,
+	U3CSetLineUIU3Ed__15_System_IDisposable_Dispose_mBD12D04E12E9C4A20C5295319F2A53486E2DD8EC,
+	U3CSetLineUIU3Ed__15_MoveNext_m3CEAE3266F236FB42D867E90A9FF6FD8CA2E6404,
+	U3CSetLineUIU3Ed__15_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m62CCD0B02BC37AF8151212C7B46F22D6D31EF07F,
+	U3CSetLineUIU3Ed__15_System_Collections_IEnumerator_Reset_m82D35F87248202C9327225D430BA13439A5FE2B6,
+	U3CSetLineUIU3Ed__15_System_Collections_IEnumerator_get_Current_m7B4F53741CB4716890E040FC7D3684CE95962B88,
+	PlotManager__ctor_m728BB55B53CFA5F5B6B18CE491BCB2665E3B1D63,
+	DialogueSticker_Start_m8E409FBE628D9A175347F57A451E8F98069723D8,
+	DialogueSticker_Update_mD853E01077D7869A24C1345DA5889614A37D6BC0,
+	DialogueSticker__ctor_mC948229D86FAF95E4FF7FD8EF1C09D14CC3A2EB1,
+};
+static const int32_t s_InvokerIndices[36] = 
+{
+	3400,
+	3400,
+	3400,
+	2440,
+	3400,
+	2752,
+	3400,
+	3250,
+	3308,
+	3400,
+	3308,
+	3400,
+	5051,
+	4978,
+	3400,
+	3400,
+	1396,
+	2438,
+	1174,
+	3400,
+	2752,
+	3400,
+	3250,
+	3308,
+	3400,
+	3308,
+	2752,
+	3400,
+	3250,
+	3308,
+	3400,
+	3308,
+	3400,
+	3400,
+	3400,
+	3400,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	36,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ Library/Bee/artifacts/emscripten/0rttl/wrapper_emar.bat
@@ -0,0 +1,4 @@
+
+@echo off
+"D:\Unity\2021.3.6f1c1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emar.bat" %* < nul
+exit %ERRORLEVEL%
